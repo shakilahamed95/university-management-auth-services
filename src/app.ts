9cd7b2a619113b34464a38a2a5c37daf576c562e
Md. Shakil Ahamed <shakilahamed.ict.iu@gmail.com>
2023-05-29 22:04:13 +0600

    cors,parser added

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response, json, urlencoded } from "express";
+import cors from "cors";
+const app: Application = express();
+const port = 3000;
+
+app.use(cors());
+
+// json parser 
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get("/", (req:Request, res:Response) => {
+  res.send("Hello World!");
+});
+
+export default app;
