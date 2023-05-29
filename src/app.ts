4dfeee40d0d1d62c7bc30fa8ceeb928c38ad332f
Md. Shakil Ahamed <shakilahamed.ict.iu@gmail.com>
2023-05-29 23:05:17 +0600

    lint,prettier and huskey installed

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,23 @@
-import express, { Application, Request, Response, json, urlencoded } from "express";
-import cors from "cors";
-const app: Application = express();
-const port = 3000;
+import express, {
+  Application,
+  Request,
+  Response,
+  json,
+  urlencoded,
+} from 'express'
+import cors from 'cors'
+const app: Application = express()
+const port = 3000
 
-app.use(cors());
+app.use(cors())
 
-// json parser 
+// json parser
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req:Request, res:Response) => {
-  res.send("Hello World!");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
 
-export default app;
+export default app
